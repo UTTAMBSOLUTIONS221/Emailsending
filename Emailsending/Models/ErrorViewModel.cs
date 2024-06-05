35c4f2d3650f671974ae4c28eb7b33c5714f36b5
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-06-05 09:02:46 +0300

    Add project files.

---
+++ Emailsending/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Emailsending.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
